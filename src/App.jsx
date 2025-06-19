2260098b5f69827f0dd8e83cc5033afc98ba5a95
kramses <124073556+kramses@users.noreply.github.com>
2025-06-20 04:38:31 +0800

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@
 // Root layout component that renders children (pages) inside the app.
 
 import "./App.css";
-
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 function App({ children }) {
-  return <>{children}</>;
+ return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage /> } />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} /> {/* Your new page */}
+    </Routes>
+  </Router>
+ )
 }
 
 export default App;
